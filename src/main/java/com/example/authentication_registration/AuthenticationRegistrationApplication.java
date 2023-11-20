40a02322004c1b94306622e50392fca5b27c8284
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2023-11-20 23:25:42 +0100

    springSecurity-jwt-app

---
+++ src/main/java/com/example/authentication_registration/AuthenticationRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.example.authentication_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationRegistrationApplication.class, args);
+
+	}
+
+}
